d832c0383a81569e15bee92ed3ba84d61e866a84
batan <batan@BOYAN_WIN11>
2022-01-25 00:39:06 +0000

    Add project files.

---
+++ project_base.cpp
@@ -0,0 +1,12 @@
+﻿// project_base.cpp : Defines the entry point for the application.
+//
+
+#include "project_base.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
